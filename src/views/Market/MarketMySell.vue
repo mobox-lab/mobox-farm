26be613b259db63f0eda2fec9dcc9797d57e021d
Tomori6657 <japancjc123@gmail.com>
2021-03-03 10:20:14 +0800

    feat: dev market

--- src/views/Market/MarketMySell.vue
+++ src/views/Market/MarketMySell.vue
@@ -1,25 +1,29 @@
 <template>
 	<div>
 		<div class="tal search vertical-children por mgt-20">
-			<span>Total pet: {{ marketPets.total }}</span>&nbsp;
+			<span>Total pet: {{ marketPetsMy.total }}</span>&nbsp;
 			<span class="search-box hide" >
 				<input class="ly-input mgl-20" type="text/" />&nbsp;
 				<img :src="require('@/assets/icon/search.png')" alt="" />
 			</span>
 		</div>
-		<div :class="marketPets.total < 6 ? 'tal' : ''"  class="mgt-20">
-			<router-link :to="'/auctionView/'+ JSON.stringify(item)"  v-for="item in marketPets.list" :key="item.uptime">
+		<div :class="marketPetsMy.total < 6 ? 'tal' : ''"  class="mgt-20">
+			<router-link :to=" item.index > 0 ? ('/auctionView/'+ JSON.stringify(item)):'###'" :class="item.index >= 0?'':'opa-6'" v-for="item in [...tempSells,...marketPetsMy.list]" :key="item.uptime">
 				<PetItem  v-bind:data="{item: item}" class="market" v-if="item.tokenId != 0 " >
-					<div class="vertical-children mgt-10" style="font-size: 18px">
-						<img src="../../assets/coin/BUSD.png" alt="" height="20"/>&nbsp;
+					<div class="vertical-children mgt-10" style="font-size: 18px;" v-if="item.index >= 0">
+						<img src="../../assets/coin/BUSD.png"  alt="" height="20"/>&nbsp;
 						<span>{{numFloor(item.nowPrice/1e9, 10000)}}</span>
+					</div>
+					<div class="vertical-children mgt-10" v-if="item.index == -1" style="font-size: 18px;">
+						<img  src="../../assets/icon/loading.png" class="rotate" height="20" alt=""  /> &nbsp;
+						<small>上架中...</small>
 					</div>
 				</PetItem>
 			</router-link>
 		</div>
 
 		<div style="margin-top: 30px" >
-			<Page :defaultPage="1" :totalPage="Math.ceil(marketPets.total / onePageCount)" :onChange="onPageChange" v-if="Math.ceil(marketPets.total / onePageCount) > 1" />
+			<Page :defaultPage="1" :totalPage="Math.ceil(marketPetsMy.total / onePageCount)" :onChange="onPageChange" v-if="Math.ceil(marketPetsMy.total / onePageCount) > 1" />
 		</div>
 	</div>
 </template>
@@ -27,7 +31,7 @@
 <script>
 import {  Page, PetItem, } from "@/components";
 import { CommonMethod } from "@/mixin";
-import { Http } from '@/utils';
+import { Http, Wallet } from '@/utils';
 import { BaseConfig } from "@/config";
 import { mapState } from "vuex";
 
@@ -38,17 +42,22 @@ export default {
 	data(){
 		return({
 			onePageCount: 15,
+			myAccount: "",
 		});
 	},
 	computed: {
 		...mapState({
-			marketPets: (state) => state.globalState.data.marketPets,
+			marketPetsMy: (state) => state.globalState.data.marketPetsMy,
+			tempSells: (state) => state.globalState.data.tempSells,
 		}),
 	},
-	created(){
-		this.getAuctionPets();
+	async created(){
+		await Wallet.ETH.init();
+		this.myAccount = await Wallet.ETH.getAccount();
+
+		this.getAuctionPetsMy();
 		timer = setInterval(()=>{
-			this.getAuctionPets();
+			this.getAuctionPetsMy();
 		}, 10000);
 	},
 	beforeDestroy(){
@@ -56,8 +65,9 @@ export default {
 	},
 	methods: {
 		//获取市场上的宠物
-		async getAuctionPets(){
-			let data = await Http.getAuctionList("eth");
+		async getAuctionPetsMy(){
+			let data = await Http.getMyAuctionList("eth", this.myAccount);
+			console.log("getAuctionPetsMy",data);
 			data.list.map(item=>{
 				if( item.tokenId != 0){
 					let {tokenName} = BaseConfig.NftCfg[item.prototype];
@@ -74,7 +84,7 @@ export default {
 				}
 				item.nowPrice = nowPrice;
 			});
-			this.$store.commit("globalState/setData", {marketPets:data});
+			this.$store.commit("globalState/setData", {marketPetsMy:data});
 		},
 		onPageChange(){
 

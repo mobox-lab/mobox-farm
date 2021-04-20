c7bc1173e862ec81a4cda8aa82792e5af7865d57
Tomori6657 <japancjc123@gmail.com>
2021-04-20 15:38:46 +0800

    new

--- src/utils/Http.js
+++ src/utils/Http.js
@@ -24,7 +24,6 @@ export default class HTTP {
 	}
 	static async buybackpool(){
 		let { data } = await this.get(`/buybackpool/amount`);
-		console.log("buybackpool", data);
 		return data;
 	}
 	static async getKeyDrop(){

b7ad071bdb1d55ebbef983328207a20ce62fc609
Tomori6657 <japancjc123@gmail.com>
2021-06-11 17:59:22 +0800

    ..

--- src/views/Airdrop/Airdrop.vue
+++ src/views/Airdrop/Airdrop.vue
@@ -62,7 +62,7 @@
 								<div class=" tal mgt-5 close vertical-children point-block por"   @click="getApyObj(item);toggleClass($event, {isLP: true})">
 									<span class="tac" style="display:inline-block;height:25px;line-height:26px;width:40px;border-radius:50px;background:#4383D7;color:#fff;font-size:14px;" >{{item.allocPoint}}X</span>
 									<span class="show-point-block mgl-5">
-										<span style="font-size:14px">APY: {{ item.apy }}</span>
+										<span style="font-size:14px">APY: {{ item.apy }}----</span>
 										<svg  viewBox="0 0 24 24" class="rotate-arrow"  height="20px" ><path fill="#94BBFF" d="M8.11997 9.29006L12 13.1701L15.88 9.29006C16.27 8.90006 16.9 8.90006 17.29 9.29006C17.68 9.68006 17.68 10.3101 17.29 10.7001L12.7 15.2901C12.31 15.6801 11.68 15.6801 11.29 15.2901L6.69997 10.7001C6.30997 10.3101 6.30997 9.68006 6.69997 9.29006C7.08997 8.91006 7.72997 8.90006 8.11997 9.29006Z"></path></svg>
 									</span>
 

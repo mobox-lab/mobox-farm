8925f891d954a052c93cf0d8d6b40d9165c9c565
joji-mb <545633740@qq.com>
2022-08-25 10:29:16 +0800

    style: format code

--- src/App.vue
+++ src/App.vue
@@ -386,6 +386,10 @@
 			<div class="mgt-10 tab-body tal" >
 				<div class="tab-panel" style="max-height:500px;overflow-x:auto;background:rgba(0,0,0,0.8);word-break: break-all">
 					<div class="mgt-20">
+						<h3 class="tac">{{$t("Notice_76")}}</h3>
+						<span v-html="$t('Notice_77')" ></span>
+					</div>
+					<div class="mgt-20">
 						<h3 class="tac">{{$t("Notice_74")}}</h3>
 						<span v-html="$t('Notice_75')" ></span>
 					</div>
@@ -671,7 +675,7 @@ export default {
 			powerTab: "v4",
 			hasReadNotice: false,
 			showMoreMenu: false,
-			noticeVersion: "4.4",
+			noticeVersion: "2022-8-25",
 			version: "2.1.1"
 		};
 	},

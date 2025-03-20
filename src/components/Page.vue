5b56655ec51f50ab1e046fa4293bb28e071f1c76
Tomori6657 <japancjc123@gmail.com>
2025-03-20 14:24:42 +0800

    docs: 📝 update user manual

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -79,4 +79,19 @@ export default {
 		}
 	},
 };
+</script><template>
+  <div class="updated-component">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdatedComponent',
+  data() {
+    return {
+      message: 'Component updated'
+    }
+  }
+}
 </script>

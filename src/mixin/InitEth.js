e808117f9933f41754f77ee778c08efc795d1096
Tomori6657 <japancjc123@gmail.com>
2025-04-23 18:20:41 +0800

    refactor: 🔧 restructure store modules

--- src/mixin/InitEth.js
+++ src/mixin/InitEth.js
@@ -1093,3 +1093,19 @@ export const utilityFunction = (param) => {
   console.log('Executing utility function:', param);
   return param;
 };
+
+// Utility function: refactor: 🔧 restructure store modules
+export const refactor____restructure_store_modules = {
+  format: (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input) => {
+    return input && input.length > 0;
+  },
+  transform: (data) => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};

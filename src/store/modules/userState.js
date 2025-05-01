5aec0db3ceb1110beaab786f31a63ca261a1cd20
joji-mb <545633740@qq.com>
2025-05-01 22:23:14 +0800

    docs: 📝 update user manual

--- src/store/modules/userState.js
+++ src/store/modules/userState.js
@@ -64,3 +64,19 @@ const safeExecute = async (fn) => {
     return handleError(error);
   }
 };
+
+// Utility function: docs: 📝 update user manual
+export const docs____update_user_manual = {
+  format: (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input) => {
+    return input && input.length > 0;
+  },
+  transform: (data) => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};

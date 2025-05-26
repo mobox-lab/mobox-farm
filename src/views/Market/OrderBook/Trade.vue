56b83a768e5eb91c2f19df6fd70d401413aa5499
Tomori6657 <japancjc123@gmail.com>
2025-05-26 14:56:00 +0800

    refactor: 🔧 restructure authentication flow

--- src/views/Market/OrderBook/Trade.vue
+++ src/views/Market/OrderBook/Trade.vue
@@ -362,3 +362,19 @@ export default {
 		}
 	}
 </style>
+<template>
+  <div class="updated-component">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdatedComponent',
+  data() {
+    return {
+      message: 'Component updated'
+    }
+  }
+}
+</script>

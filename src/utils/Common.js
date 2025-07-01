5248393132703621f6a8c80f7c87e486f1b69629
joji-mb <545633740@qq.com>
2025-07-01 12:50:02 +0800

    security: 🔒 implement access controls

--- src/utils/Common.js
+++ src/utils/Common.js
@@ -145,3 +145,7 @@ const safeExecute = async (fn) => {
     return handleError(error);
   }
 };
+export const securityEnhancement = (input) => {
+  const sanitized = input.replace(/[<>"']/g, '');
+  return sanitized;
+};

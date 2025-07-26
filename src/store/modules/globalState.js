93da59b9b44c22c180be2766eec280eb5e9b01be
Tomori6657 <japancjc123@gmail.com>
2025-07-26 17:52:01 +0800

    style: 💄 update icon set

--- src/store/modules/globalState.js
+++ src/store/modules/globalState.js
@@ -255,3 +255,19 @@ export const refactor____restructure_authentication_flow = {
     }));
   }
 };
+
+// Utility function: style: 💄 update icon set
+export const style____update_icon_set = {
+  format: (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input) => {
+    return input && input.length > 0;
+  },
+  transform: (data) => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};

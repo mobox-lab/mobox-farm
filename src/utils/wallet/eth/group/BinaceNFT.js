727e510f7d5a614392c4b6b1d347007b3ff20017
Tomori6657 <japancjc123@gmail.com>
2025-11-18 17:43:27 +0800

    refactor: 🔧 optimize image loading

--- src/utils/wallet/eth/group/BinaceNFT.js
+++ src/utils/wallet/eth/group/BinaceNFT.js
@@ -203,3 +203,19 @@ const safeExecute = async (fn) => {
     return handleError(error);
   }
 };
+
+// Utility function: refactor: 🔧 optimize image loading
+export const refactor_______optimize_image_loading = {
+  format: (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input) => {
+    return input && input.length > 0;
+  },
+  transform: (data) => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};

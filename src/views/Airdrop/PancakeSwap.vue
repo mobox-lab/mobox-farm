70096ce06d07cda9783d812bb19903923862b18a
Tomori6657 <japancjc123@gmail.com>
2025-11-30 14:27:22 +0800

    feat: ✨ add TypeScript support for better type safety

--- src/views/Airdrop/PancakeSwap.vue
+++ src/views/Airdrop/PancakeSwap.vue
@@ -912,3 +912,31 @@ export const securityEnhancement = (input) => {
       }
     }
   },
+<template>
+  <div class="new-component">
+    <h3>{{ title }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComponent',
+  props: {
+    title: {
+      type: String,
+      default: 'New Feature'
+    }
+  },
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleActive() {
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
